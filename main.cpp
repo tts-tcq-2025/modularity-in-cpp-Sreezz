1b25020b79a72884a2d4915e73d8a18190e791cb
Sreezz <65155642+Sreezz@users.noreply.github.com>
2025-08-11 23:37:53 +0530

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <assert.h>
-#include <colorpair.h>
+#include "colorpair.h"
 
 void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) 
 {
