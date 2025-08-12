ea20bb6bce8c293f9e54e13aadf3f54223718aed
Sreezz <65155642+Sreezz@users.noreply.github.com>
2025-08-12 09:24:25 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,6 @@
 #include <iostream>
 #include <assert.h>
-#include "colorpair.h"
-
-void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) 
-{
-    ColorPair pair = GetColorFromPairNumber(pairNumber);
-    printf("Got pair %s\n", ToString(pair));
-    assert(pair.major == expectedMajor);
-    assert(pair.minor == expectedMinor);
-}
-
-void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber) 
-{
-    int pairNumber = GetPairNumberFromColor(major, minor);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
+#include "Test_colorpair.h"
 
 int main() 
 {
